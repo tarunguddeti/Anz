139a6e51e4afb7f5c4ef6f79d96663de0b5aa74b
Tarun <gtarun1991@gmail.com>
2018-10-09 22:11:10 +1100

    Added code to read and calculate the losses and gains based on closing share prices.

---
+++ Anz.java
@@ -0,0 +1,87 @@
+import java.util.Scanner;
+
+class Anz {
+    static Scanner scanner = new Scanner(System.in);
+    static float closingSharePrices[];
+
+    public static void main(String args[]) {
+
+        // Loop infinitely to until the user opts for exit
+        while (true) {
+            // Present user the menu with different choises
+            System.out.println("Menu");
+            System.out.println("Press 1 - Read and Validate Data");
+            System.out.println("Press 2 - Calculate total Gain and Losses");
+            System.out.println("Press 3 - Exit");
+            // Read user choise
+            int menu = scanner.nextInt();
+            // Based on the users choise call the appropriate function
+            switch (menu) {
+            case 1:
+                ReadAndValidateData();
+                break;
+            case 2:
+                CalculateTotalLossesAndGains();
+                break;
+            case 3:
+                System.out.print("Exiting bye !!");
+                System.exit(0);
+            default:
+                System.out.print("Please enter the valid option");
+
+            }
+        }
+    }
+
+    // This method reads the data and validates it
+    private static void ReadAndValidateData() {
+        System.out.println("Please enter for how many days to take closing share prices ?");
+        int numberOfDays = 0;
+        while (true) {
+            numberOfDays = scanner.nextInt();
+            if (numberOfDays <= 0) {
+                // Validate if the user entered a positive number for the number of days
+                System.out.print("The number of days should be a positive number greater than 0");
+            } else {
+                break;
+            }
+        }
+        // Instantiate closingSharePrices based on the number of days
+        closingSharePrices = new float[numberOfDays];
+        for (int i = 0; i < numberOfDays; i++) {
+            try {
+                System.out.println("Day " + (i + 1));
+                closingSharePrices[i] = scanner.nextFloat();
+            } catch (Exception e) {
+                System.out.println("An Exception has occured as you have entered invalid data");
+                break;
+            }
+        }
+    }
+
+    /*
+     * This method calculates the total losses and gains for the previously read /
+     * data
+     */
+    private static void CalculateTotalLossesAndGains() {
+        System.out.println("Calculating total losses and gains based on the given data");
+        float totalLosses = 0;
+        float totalGains = 0;
+
+        if (closingSharePrices.length == 0) {
+            System.out.println("Please choose option 1 to enter the data first");
+            return;
+        }
+
+        for (int i = 0; i < closingSharePrices.length - 1; i++) {
+            if (closingSharePrices[i] > closingSharePrices[i + 1]) {
+                totalLosses = totalLosses + (closingSharePrices[i + 1] - closingSharePrices[i]);
+            } else {
+                totalGains = totalGains + Math.abs(closingSharePrices[i] - closingSharePrices[i + 1]);
+            }
+        }
+
+        System.out.println("Total Losses : " + totalLosses);
+        System.out.println("Total Gains : " + totalGains);
+    }
+}
